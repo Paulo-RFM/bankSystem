5cfd2535aaffab389e62c1948b8846d7c3cbebb7
Paulo-RFM <paulo.rfm.edu@gmail.com>
2024-05-20 22:48:12 -0300

    correção de alguns endpoints

---
+++ myBank/Cassandra_client.py
@@ -0,0 +1,27 @@
+from cassandra.cluster import Cluster
+
+class CassandraClient:
+    def __init__(self):
+        self.cluster = Cluster(['127.0.0.1'])
+        self.session = self.cluster.connect()
+        self.session.execute(f"CREATE KEYSPACE IF NOT EXISTS {'mykeyspace'} WITH replication = {{ 'class': 'SimpleStrategy', 'replication_factor': '1' }}")
+        self.session.set_keyspace('mykeyspace')
+        self.create_tables()
+
+    def create_tables(self):
+        self.session.execute("""
+            CREATE TABLE IF NOT EXISTS accounts (
+                account_number TEXT PRIMARY KEY,
+                account_holder TEXT,
+                bank_id TEXT,
+                balance FLOAT,
+                created_at TIMESTAMP
+            )
+        """)
+
+
+    def execute(self, query, parameters=None):
+        if parameters:
+            return self.session.execute(query, parameters)
+        return self.session.execute(query)
+
